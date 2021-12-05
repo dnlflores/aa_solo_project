0db234407809d99f2b2130f782d94965302258c8
Daniel Flores <daniel.flores4930@gmail.com>
2021-12-05 15:41:53 -0800

    added some css and functionality to login page

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as sessionActions from '../../store/session';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return (
+        <Redirect to="/" />
+    )
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ credential, password }))
+            .catch(async res => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            })
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="error-list">
+                <ul>
+                    {errors.map((error, index) => <li key={index}>{error}</li>)}
+                </ul>
+            </div>
+            <div className="login-div">
+                <label className="username-label">
+                    Username/Email: 
+                    <input
+                        type='text'
+                        value={credential}
+                        onChange={event => setCredential(event.target.value)}
+                        required
+                        id="username-input"
+                    />
+                </label>
+                <label className="password-label">
+                    Password: 
+                    <input 
+                        type="password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                        required
+                        id="password-input"
+                    />
+                </label>
+                <button type="submit" id="login-button">Log In</button>
+            </div>
+        </form>
+    )
+}
+
+export default LoginFormPage;
