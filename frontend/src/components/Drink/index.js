76ae4b8738e715a5ae239fd2aec3882d155d0ca0
Daniel Flores <daniel.flores4930@gmail.com>
2021-12-09 16:02:00 -0800

    initial delete functionality working

--- frontend/src/components/Drink/index.js
+++ frontend/src/components/Drink/index.js
@@ -1,8 +1,10 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import EditPopup from '../EditPopup';
+import { deleteOneDrink } from '../../store/drink';
 
 const Drink = ({ drink }) => {
+    const dispatch = useDispatch();
     const sessionUser = useSelector(state => state.session.user);
     const [buttonPopup, setButtonPopup] = useState(false);
     
@@ -17,6 +19,9 @@ const Drink = ({ drink }) => {
                         <>
                             <button className="button button--janus edit-drink-button" onClick={event => setButtonPopup(true)}><span>Edit</span></button>
                             <EditPopup trigger={buttonPopup} setTrigger={setButtonPopup} drink={drink} />
+                            <button className="btn button-3" onClick={event => {
+                                dispatch(deleteOneDrink(drink));
+                            }} >Delete</button>
                         </>}
                 </div>
                 <p className="drink-description" id={`beer-${drink.id}-description`}>{drink.description}</p>
